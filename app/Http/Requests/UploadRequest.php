0b328e5db471b32f2e6f69cc51e23b4789f49df5
gsfranzoni <gsfranzoni@gmail.com>
2022-11-11 11:28:19 -0300

    feat (backend): validate file size and mime types

--- app/Http/Requests/UploadRequest.php
+++ app/Http/Requests/UploadRequest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UploadRequest extends FormRequest
 {
+    public const MAX_FILE_SIZE_5_MB = 1024 * 1024 * 5;
+
     public function authorize(): bool
     {
         return true;
@@ -14,7 +16,7 @@ class UploadRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'file' => 'required|file',
+            'file' => 'required|file|mimes:jpg,jpeg,png,gif|max:' . self::MAX_FILE_SIZE_5_MB,
         ];
     }
 }
